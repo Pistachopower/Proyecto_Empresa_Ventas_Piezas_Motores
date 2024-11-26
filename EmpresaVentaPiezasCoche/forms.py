2236bf4bcb407b61e275f59a7f5dc112b5b29369
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-11-26 12:24:29 +0100

    Se comienza la planificación del formulario

---
+++ EmpresaVentaPiezasCoche/forms.py
@@ -0,0 +1,8 @@
+#debes colocar esto para trabajar con tus formularios
+from django import forms
+
+"""
+con class creamos un formulario usando forms.Form
+"""
+#class ProveedorForm(forms.Form):
+    #proveedor
